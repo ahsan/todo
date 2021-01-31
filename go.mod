0018934d353d7c2f551e43c915a780ea8d2d690b
Abdullah Ahsan <ahsan.abdulah@gmail.com>
2021-01-31 22:16:02 +0100

    Cli setup

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ahsan/todo
 
 go 1.15
 
-require github.com/urfave/cli/v2 v2.3.0 // indirect
+require github.com/urfave/cli/v2 v2.3.0
--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	(&cli.App{}).Run(os.Args)
+	app := &cli.App{
+		Name:  "todo",
+		Usage: "keep track of your todo items on the cli",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Hello world!")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
